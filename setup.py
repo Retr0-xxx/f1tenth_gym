5432a9585571e0a26dbbfdc8370a960229e66b77
Eric Tang <134814410+Retr0-xxx@users.noreply.github.com>
2024-09-14 11:30:25 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
 from setuptools import setup
 
-setup(name='f110_gym',
-      version='0.2.1',
-      author='Hongrui Zheng',
-      author_email='billyzheng.bz@gmail.com',
-      url='https://f1tenth.org',
-      package_dir={'': 'gym'},
-      install_requires=['gym==0.19.0',
-		        'numpy<=1.22.0,>=1.18.0',
-                        'Pillow>=9.0.1',
-                        'scipy>=1.7.3',
-                        'numba>=0.55.2',
-                        'pyyaml>=5.3.1',
-                        'pyglet<1.5',
-                        'pyopengl']
-      )
+setup(
+    name='f110_gym',
+    version='0.2.1',
+    author='Hongrui Zheng',
+    author_email='billyzheng.bz@gmail.com',
+    url='https://f1tenth.org',
+    packages=['f110_gym'],  # Ensure your actual package name here
+    package_dir={'f110_gym': 'gym'},  # Adjust path to your package correctly
+    install_requires=[
+        'gym==0.19.0',
+        'numpy>=1.18.0,<1.23.0',  # Broaden the version range if possible
+        'Pillow>=9.0.1',
+        'scipy>=1.7.3',
+        'numba>=0.55.2',
+        'pyyaml>=5.3.1',
+        'pyglet<1.5',
+        'pyopengl'
+    ],
+    extras_require={
+        'dev': [
+            'pytest>=5.4.1',
+            'coverage>=5.1'
+        ]
+    }
+)
